4aa0b00a18b533d325380c78f1394fd0975634f6
SaadMahmood1122 <sk8066709@gamil.com>
2025-03-18 19:24:58 +0500

    added filters to the tasks

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksService } from '../tasks.service';
@@ -14,7 +14,24 @@ export class TasksListComponent {
   // Injecting TaskService
   private taskService = inject(TasksService);
   selectedFilter = signal<string>('all');
-  tasks = this.taskService.allTask;
+  tasks = computed(() => {
+    switch (this.selectedFilter()) {
+      case 'OPEN':
+        return this.taskService
+          .allTask()
+          .filter((task) => task.status === 'OPEN');
+      case 'IN_PROGRESS':
+        return this.taskService
+          .allTask()
+          .filter((task) => task.status === 'IN_PROGRESS');
+      case 'DONE':
+        return this.taskService
+          .allTask()
+          .filter((task) => task.status === 'DONE');
+      default:
+        return this.taskService.allTask(); // For 'all' and any other case
+    }
+  });
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, signal } from '@angular/core';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +15,13 @@ export class TasksService {
       status: 'OPEN',
     };
     this.task.update((oldeTask) => [...oldeTask, newTask]);
+  }
+
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.task.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
   }
 }
